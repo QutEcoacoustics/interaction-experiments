e9196c60a154afeb7494b1a26fdcb7e0214da2cb
Charles Alleman <charles@alleman.com.au>
2019-06-28 16:43:32 +1000

    Working, but data output is wrong

--- content/experiments/fcs/experiment.js
+++ content/experiments/fcs/experiment.js
@@ -199,7 +199,8 @@ function experimentInit() {
         data: () => ({
             trialName: "tutorial",
             condition: getCondition()
-        })
+        }),
+        tagging_options: {label: "Label Here", choices: ['Option 1', 'Option 2', 'Option 3']}
     };
 
     var exploreTask = {
